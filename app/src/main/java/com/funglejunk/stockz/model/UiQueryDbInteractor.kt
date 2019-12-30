1f1dea90f99f613c33d3a7acd7072268f2419207
micotom <funglejunk.development@googlemail.com>
2019-12-30 22:02:25 +0100

    removed unused imports

--- app/src/main/java/com/funglejunk/stockz/model/UiQueryDbInteractor.kt
+++ app/src/main/java/com/funglejunk/stockz/model/UiQueryDbInteractor.kt
@@ -1,10 +1,8 @@
 package com.funglejunk.stockz.model
 
 import androidx.sqlite.db.SimpleSQLiteQuery
-import arrow.core.Either
 import arrow.fx.IO
 import arrow.fx.extensions.fx
-import com.funglejunk.stockz.data.Etf
 import com.funglejunk.stockz.data.UiEtfQuery
 import com.funglejunk.stockz.repo.db.XetraDbEtf
 import com.funglejunk.stockz.repo.db.XetraDbInterface
