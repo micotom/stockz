326043efd40b70ea7520acdb4eb4b31f193ffbd2
micotom <funglejunk.development@googlemail.com>
2019-12-31 15:28:41 +0100

    added navigation to detail page

--- app/src/main/java/com/funglejunk/stockz/ui/FavouritesFragment.kt
+++ app/src/main/java/com/funglejunk/stockz/ui/FavouritesFragment.kt
@@ -6,20 +6,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.funglejunk.stockz.R
 import com.funglejunk.stockz.data.Etf
 import com.funglejunk.stockz.model.FavouritesViewModel
 import com.funglejunk.stockz.ui.adapter.FavouritesAdapter
 import kotlinx.android.synthetic.main.etf_list_fragment.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import timber.log.Timber
 
 class FavouritesFragment : Fragment() {
 
     private val viewModel: FavouritesViewModel by viewModel()
 
     private val itemClickListener: (Etf) -> Unit = { etf ->
-        Timber.d("clicked: ${etf.name}") // TODO navigate
+        findNavController().navigate(FavouritesFragmentDirections.favToDetailAction(etf))
     }
 
     override fun onCreateView(
@@ -32,8 +32,7 @@ class FavouritesFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
         viewModel.favouritesData.observe(viewLifecycleOwner, Observer { etfList ->
-            recycler_view.adapter =
-                FavouritesAdapter(etfList, itemClickListener)
+            recycler_view.adapter = FavouritesAdapter(etfList, itemClickListener)
         })
     }
 
