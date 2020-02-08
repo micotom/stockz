f32589b602fabb7b3ed70d7df0f2714a4f508078
micotom <funglejunk.development@googlemail.com>
2020-02-08 15:00:04 +0100

    added lc callbacks and reading args

--- app/src/main/java/com/funglejunk/stockz/ui/AssetDetailFragment.kt
+++ app/src/main/java/com/funglejunk/stockz/ui/AssetDetailFragment.kt
@@ -1,6 +1,27 @@
 package com.funglejunk.stockz.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.funglejunk.stockz.R
+import timber.log.Timber
 
 class AssetDetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.asset_detail_fragment, container, false)
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val assetSummaryArg = arguments?.let { AssetDetailFragmentArgs.fromBundle(it).assetSummary }
+        assetSummaryArg?.let { assetSummary ->
+            Timber.d(assetSummary.toString())
+        }
+    }
+
 }
